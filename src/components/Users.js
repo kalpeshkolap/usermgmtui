84972e23541ab05c3938462b3e3bb8a71af09612
kalpesh <53437883+kalpeshkolap@users.noreply.github.com>
2024-05-20 13:53:51 +0530

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -57,7 +57,7 @@ function Users() {
 
   return (
     <div className="container-fluid">
-      <div className="p-3 bg-primary text-white text-center">
+      <div className="p-3 bg-warning text-white text-center">
         <h1>User Details Management System</h1>
         <p>The User Details Management System</p>
       </div>
